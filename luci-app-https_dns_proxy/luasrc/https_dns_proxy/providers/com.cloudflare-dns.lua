33d09934f5b779521d5c93f572aaa20bd7f16fa4
Stan Grishin <stangri@melmac.net>
2019-12-03 12:32:16 -0700

    https_dns_proxy: binary support

--- luci-app-https_dns_proxy/luasrc/https_dns_proxy/providers/com.cloudflare-dns.lua
+++ luci-app-https_dns_proxy/luasrc/https_dns_proxy/providers/com.cloudflare-dns.lua
@@ -1,6 +1,6 @@
 return {
 	name = "Cloudflare",
 	label = _("Cloudflare"),
-	url_prefix = "https://cloudflare-dns.com/dns-query?ct=application/dns-json&",
+	url_prefix = "https://cloudflare-dns.com/dns-query",
 	bootstrap_dns = "1.1.1.1,1.0.0.1"
 }
